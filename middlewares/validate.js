060a0c912134fe27280e42bb6a5d1d58e9e7d841
Marcelearn <jaworskimarcel@gmail.com>
2023-01-23 14:42:11 +0100

    validate-commentar added

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -1,7 +1,7 @@
 /*Bei der Verwendung von ajv validieren wir die Daten in dem Moment, in dem sie empfangen werden*/ 
 import Ajv from "ajv";
 
-const ajv = new Ajv({ allErrors: true });//wir deklarieren eine neue Instanz
+const ajv = new Ajv({ allErrors: true });//wir deklarieren eine neue Instanz von Ajv
 
 const validate = (schema) => {
     /*Ajv kompiliert Schemas zu Funktionen und fängt sie in allen Fällen ab (unter Verwendung des Schemas selbst als Schlüssel in einer Map), sodass das nächste Mal, wenn dasselbe Schemaobjekt verwendet wird, es nicht erneut kompiliert wird*/ 
@@ -10,8 +10,9 @@ const validate = (schema) => {
     return (req, res, next) => {
 
         const valid = test(req.body);
+        //Wenn es nicht validiert wird, antworten wir mit Status 400 und senden den Fehler
         if (!valid) return res.status(400).json(test.errors);
-
+        //Wenn validiert, ermöglicht next die Ausführung der folgenden Aktionen
         next();
     };
 }
